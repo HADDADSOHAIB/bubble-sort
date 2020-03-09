1feb337237c249e88c8ee0d4fd4d79724853f829
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-09 16:53:19 +0100

    fix linter issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -23,10 +23,10 @@ def bubble_sort_by(arr)
   arr.length.times do
     (arr.length - 1).times do
       block_vlaue = yield(arr[i], arr [i + 1])
-      if  block_vlaue > 0
+      if block_vlaue.positive?
         temp = arr[i + 1]
         arr[i + 1] = arr[i]
-        arr[i]=temp
+        arr[i] = temp
       end
       i += 1
     end
@@ -35,4 +35,4 @@ def bubble_sort_by(arr)
   arr
 end
 
-p bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length }
+p bubble_sort_by(%w( hi hello hey)) { |left, right| left.length - right.length }
